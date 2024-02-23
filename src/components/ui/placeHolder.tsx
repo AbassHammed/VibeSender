304122c7afb5d792a7e570d0c6dc2d7cc107ee50
Hammed <abasshammedola@gmail.com>
2024-02-23 21:34:40 +0100

    Nothing Special

--- src/components/ui/placeHolder.tsx
+++ src/components/ui/placeHolder.tsx
@@ -7,11 +7,7 @@ interface PlaceholderProps {
 
 const Placeholder: React.FC<PlaceholderProps> = ({ description, imagePath }) => (
   <div className="w-full h-full flex flex-col items-center justify-center">
-    <img
-      src={imagePath}
-      alt="Placeholder"
-      className="mb-4 max-w-[60%] max-h-[60%] items-center"
-    />
+    <img src={imagePath} alt="Placeholder" className="mb-4 max-w-[60%] max-h-[60%] items-center" />
     <p className="text-center">{description}</p>
   </div>
 );
