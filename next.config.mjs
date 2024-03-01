e8a6abd1f44dd942cbe66b2ef8183a66307fce79
Hammed <abasshammedola@gmail.com>
2024-03-01 22:32:21 +0100

    Just started the Notifications ui i won't be going with a page but a sheet board

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  name: 'preset-default',
-                  params: {
-                    override: {
-                      prefixIds: false,
-                      prefixClassNames: false,
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        },
-      ],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
